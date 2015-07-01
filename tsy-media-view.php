d09990988ec22bb4b0d51fa5c954ec776874a292
Gordon W <gordjw@gmail.com>
2015-07-01 23:45:45 +1000

    Added stylesheet

--- tsy-media-view.php
+++ tsy-media-view.php
@@ -14,10 +14,15 @@ class Media_View
 		add_action( 'admin_menu', array( &$this, 'add_menu_pages' ) );
 		add_action( 'init', array( &$this, 'register_taxonomies' ) );
 
+		add_action( 'admin_enqueue_scripts', array( &$this, 'enqueue_scripts' ) );
 
 		add_action( 'wp_ajax_get_attachments_filtered', array( &$this, 'get_attachments_filtered' ) );
 	}
 
+	public function enqueue_scripts() {
+		wp_enqueue_style( 'media-view', plugins_url( 'media-view.css', __FILE__ ) );
+		wp_enqueue_script( 'wp-util' );
+	}
 
 	public function register_taxonomies() {
 		// Use attachment_category and attachment_tag, because that's we've already got in quite a few sites, so let's re-use that
@@ -89,12 +94,10 @@ class Media_View
 
 
 	public function media_view_page() {
-
 		add_filter( 'posts_clauses', array( &$this, 'add_taxonomy_terms_to_attachments' ) );
 
 		echo '<h1>Media</h1>';
-
-		echo '<div class="sidebar">';
+		echo '<div class="attachment-sidebar">';
 
 		$args = array(
 			'hide_empty'	=> false,
@@ -104,7 +107,7 @@ class Media_View
 		$tags = get_terms( 'attachment_tag', $args );
 
 
-		echo '<ul>';
+		echo '<ul class="list">';
 		foreach( $categories as $category ) {
 			printf( '<li><a href="#" class="filter-link" data-category="%s">%s</a>', $category->term_id, $category->name );
 
@@ -117,24 +120,25 @@ class Media_View
 		}
 		printf( '</ul>' );
 
-		printf( '</div><div class="attachment_list"><h2>Attachments</h2>' );
-
-		printf( '<div class="attachment_container"></div>' );
-
+		printf( '</div><div class="attachment-list"><h2>Attachments</h2>' );
+		printf( '<div class="attachment-container"></div>' );
 		printf( '</div>' );
 
 		?>
 		<script type="text/html" id="tmpl-attachment-list">
-			<p>Att</p>
 			<ul>
+				<# _.each( data, function( a ) { #>
+					<li><a href="#" class="detail-link" data-id="{{ a.ID }}">{{ a.post_title }}</a></li>
+				<# }); #>
 			</ul>
 		</script>
 		<script type="text/html" id="tmpl-attachment-detail">
+			<h3>{{ data.post_title }}</h3>
+			<img src="{{ data.guid }}">
 		</script>
 		<script>
 		jQuery( document ).ready( function($) {
 			var attachments;
-			var my_template = wp.template( 'attachment_list' );
 			console.log( wp );
 
 			$('.sidebar').on( 'click', 'a.filter-link', function(e) {
@@ -144,8 +148,6 @@ class Media_View
 				$('a.filter-link').removeClass('selected');
 				$(this).addClass('selected');
 
-				$('.attachment_container').html('');
-
 				var data = {
 					'action': 'get_attachments_filtered',
 					'category': category,
@@ -153,26 +155,20 @@ class Media_View
 				};
 
 				$.post( ajaxurl, data, function(res) {
-					attachments = $.parseJSON(res);
-
-
-					var attachment_list = my_template( attachments );
-
-					/*if( attachments.length > 0 ) {
-						var attachment_list = '';
-						$(attachments).each( function( i ) {
-							console.log( attachments[i] );
-							attachment_list = attachment_list + '<li>' + attachments[i].ID + '</li>';
-						});
-					} else {
-						var attachment_list = '<p>No attachments found in category: ' + category;
-						if( undefined != tag )
-							attachment_list = attachment_list + ' and tag: ' + tag;
-						attachment_list = attachment_list + '</p>';
-					}*/
-					$('.attachment_container').html(attachment_list);
+					console.log(res);
+					var template = wp.template( 'attachment-list' );
+					$('.attachment-container').html( template( $.parseJSON( res ) ) );
 				});
 				
+				e.preventDefault();
+			});
+
+			$('.attachment-container').on( 'click', 'a.detail-link', function(e) {
+				$('a.detail-link').removeClass('selected');
+				$(this).addClass('selected');
+
+				console.log( $(this).attr('data-id') );
+
 				e.preventDefault();
 			});
 		});
